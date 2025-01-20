f452f614f46f11bf65f15ddbff9acb04a705ac39
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-01-20 17:17:59 +0600

    password validation complete in register page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,39 @@
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { authContext } from "../../AuthProvider/AuthProvider";
 
 export default function Register() {
+  const { handleRegister } = useContext(authContext);
+  const [error, setError] = useState("");
+  const handleForm = (e) => {
+    e.preventDefault();
+    setError("");
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const conPassword = e.target.conPassword.value;
+    const photo = e.target.photo.value;
+    // console.log(name, email, password, conPassword, photo);
+
+    if (password !== conPassword) {
+      setError("Password didn't match.");
+      return;
+    }
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!regex.test(password)) {
+      setError(
+        "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character."
+      );
+      return;
+    }
+
+    handleRegister(email, password);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form className="card-body">
+        <form onSubmit={handleForm} className="card-body">
           <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
@@ -41,6 +70,7 @@ export default function Register() {
               required
             />
           </div>
+
           <div className="form-control">
             <label className="label">
               <span className="label-text">Password</span>
@@ -49,6 +79,18 @@ export default function Register() {
               type="password"
               name="password"
               placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              name="conPassword"
+              placeholder="confirm password"
               className="input input-bordered"
               required
             />
@@ -65,7 +107,9 @@ export default function Register() {
           </div>
 
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Register</button>
+            <button className="btn bg-blue-950 text-white hover:bg-blue-800">
+              Register
+            </button>
           </div>
           <label className="label font-semibold">
             <NavLink to="/login" href="#" className="label-text-alt">
@@ -74,6 +118,7 @@ export default function Register() {
             </NavLink>
           </label>
         </form>
+        {error && <p className="text-red-600 text-center pb-7 px-4">{error}</p>}
       </div>
     </div>
   );
